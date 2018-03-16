a6da4fecb0812704ce26e8849257c74622a9e40b
jhosepv13 <jhosepv13@hotmail.com>
2018-03-16 15:38:09 -0500

    Subiendo Parte 3

--- app/src/main/java/pe/bazan/jhosep/com/laboratoriocalificado/OrderActivity.java
+++ app/src/main/java/pe/bazan/jhosep/com/laboratoriocalificado/OrderActivity.java
@@ -1,6 +1,14 @@
 package pe.bazan.jhosep.com.laboratoriocalificado;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -132,8 +140,29 @@ public class OrderActivity extends AppCompatActivity {
                 });
         alertDialog.show();
 
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        // Notification
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("Pizza Hut")
+                .setContentText("Su pedido está en camino")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+
+        // Notification manager
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+
+         }
 
     }
-}
+
+
 
